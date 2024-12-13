6cc228261d896041ecedaa9d4cb171ec3f8d0d1b
GabrielNebraska <gabrielviniciuscorrea204@gmail.com>
2024-12-13 08:41:49 -0300

    feat de verdade

--- src/components/navbar/HeaderComp.vue
+++ src/components/navbar/HeaderComp.vue
@@ -1,47 +1,32 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from 'vue-router'
 </script>
 
-
 <template>
- <nav class="navbar navbar-expand-lg bg-body-tertiary">
-   <div class="container-fluid">
-   
-
-
-     <div class="collapse navbar-collapse" id="navbarNav">
-       <ul class="navbar-nav">
-         <li class="nav-item">
-           <RouterLink class="nav-link active" aria-current="page" to="/" style="color: white;">Home</RouterLink>
-         </li>
-         <li class="nav-item">
-           <RouterLink class="nav-link" to="/categorias" style="margin-left: 30%;">Categorias</RouterLink>
-         </li>
-       </ul>
-     </div>
-   </div>
- </nav>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">Nemovies</RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/categorias">Categorias</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
-
-
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Sofia+Sans:ital,wght@0,1..1000;1,1..1000&display=swap');
-.container-fluid{
- background-color: #09005e;
- padding: 1% 0 1% 5%;
- margin: 0;
- font-family: 'Sofia Sans';
-}
-.nav-link{
- color: white;
-
-
-
-
-}
-
-
-</style>
-
-
-
